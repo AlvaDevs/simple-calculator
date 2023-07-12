18871ce2914340ee2c05b1e53d1f347eaddbb4c0
AlvaNeedsFood <aalvarez.contact05@gmail.com>
2023-07-12 12:30:48 -0500

    First change

--- simple-calculator/main.cpp
+++ simple-calculator/main.cpp
@@ -2,13 +2,48 @@
 //  main.cpp
 //  simple-calculator
 //
-//  Created by Hope Alvarez on 12/07/23.
+//  Created by Alvaro Alvarez on 13/07/23.
 //
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    int option = 0;
+    float a = 0, b = 0;
+
+//    Welcome
+    cout << "Welcome to a small project of a very simple calculator... \nHere you will only be able to do the four basic operations and with only two numbers, but I hope you enjoy it. \nFirst thing first, what operation do you want to do? \n\n";
+    
+//    Operations
+    cout << "Select an option: \n1. Addition \n2. Subtraction \n3. Multiplication \n4. Division \n: ";
+    cin >> option;
+    
+    if (option >= 1 && option <= 4) {
+//    Numbers
+        cout << "Perfect, now tell me the two numbers to do the operation (in their order from left to rigth) \na: ";
+        cin >> a;
+        cout << "b: ";
+        cin >> b;
+        
+//    Time to calculate!
+        switch (option) {
+            case 1:
+                cout << "I think the result you were looking for was this: " << a + b << "\n";
+                break;
+            case 2:
+                cout << "I think the result you were looking for was this: " << a - b << "\n";
+                break;
+            case 3:
+                cout << "I think the result you were looking for was this: " << a * b << "\n";
+                break;
+            case 4:
+                cout << "I think the result you were looking for was this: " << a / b << "\n";
+                break;
+        }
+    } else {
+        cout << "That option isn't in our possibilities at the moment, sorry for the inconvenience :( \n";
+    }
     return 0;
 }
