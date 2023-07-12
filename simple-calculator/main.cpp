b878177e6fa88ca8afe104c741a65f45e8d9f3ae
AlvaNeedsFood <aalvarez.contact05@gmail.com>
2023-07-12 01:51:07 -0500

    Initial Commit

---
+++ simple-calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  simple-calculator
+//
+//  Created by Hope Alvarez on 12/07/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
